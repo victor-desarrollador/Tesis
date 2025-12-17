113a4087049839c64fb54b8bac52d42a1b24b4eb
Victor_Enrqiue_Molina <119140911+victor-desarrollador@users.noreply.github.com>
2025-12-17 15:26:47 -0300

    actualizacion
    m

---
+++ client/.next/dev/static/chunks/src_app_auth_signin_page_tsx_ed1f70d3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a789e2b4._.js",
+  "static/chunks/src_components_cc20c687._.js"
+],
+    source: "dynamic"
+});
